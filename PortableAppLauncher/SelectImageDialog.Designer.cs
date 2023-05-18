11dab78146191c5c6d3369a4e298ead3c680e5d3
François Saura <franois.saura@gmail.com>
2023-05-18 21:27:30 +0200

    Creating SelectImageDialog to let user pick an icon between icon[] collection

---
+++ PortableAppLauncher/SelectImageDialog.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PortableAppLauncher
+{
+    partial class SelectImageDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.PanelControls = new System.Windows.Forms.Panel();
+            this.BTN_Cancel = new System.Windows.Forms.Button();
+            this.BTN_OK = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.PanelControls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelControls
+            // 
+            this.PanelControls.Controls.Add(this.BTN_Cancel);
+            this.PanelControls.Controls.Add(this.BTN_OK);
+            this.PanelControls.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PanelControls.Location = new System.Drawing.Point(0, 171);
+            this.PanelControls.Name = "PanelControls";
+            this.PanelControls.Size = new System.Drawing.Size(511, 35);
+            this.PanelControls.TabIndex = 0;
+            // 
+            // BTN_Cancel
+            // 
+            this.BTN_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.BTN_Cancel.Location = new System.Drawing.Point(12, 6);
+            this.BTN_Cancel.Name = "BTN_Cancel";
+            this.BTN_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Cancel.TabIndex = 1;
+            this.BTN_Cancel.Text = "Cancel";
+            this.BTN_Cancel.UseVisualStyleBackColor = true;
+            this.BTN_Cancel.Click += new System.EventHandler(this.BTN_Cancel_Click);
+            // 
+            // BTN_OK
+            // 
+            this.BTN_OK.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BTN_OK.Enabled = false;
+            this.BTN_OK.Location = new System.Drawing.Point(424, 6);
+            this.BTN_OK.Name = "BTN_OK";
+            this.BTN_OK.Size = new System.Drawing.Size(75, 23);
+            this.BTN_OK.TabIndex = 0;
+            this.BTN_OK.Text = "OK";
+            this.BTN_OK.UseVisualStyleBackColor = true;
+            this.BTN_OK.Click += new System.EventHandler(this.BTN_OK_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(511, 171);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // SelectImageDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 206);
+            this.ControlBox = false;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.PanelControls);
+            this.Name = "SelectImageDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SelectImageDialog";
+            this.Load += new System.EventHandler(this.SelectImageDialog_Load);
+            this.PanelControls.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel PanelControls;
+        private Button BTN_OK;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button BTN_Cancel;
+    }
+}
---
+++ PortableAppLauncher/SelectImageDialog.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortableAppLauncher
+{
+    public partial class SelectImageDialog : Form {
+
+        private Icon[] IconList;
+
+        public Icon? SelectedIcon = null;
+
+        public string Title {
+            get {
+                return this.Text;
+            }
+            set {
+                this.Text = value;
+            }
+        }
+
+        public SelectImageDialog(Icon[] iconList) {
+            InitializeComponent();
+            this.IconList = iconList;
+        }
+
+        private void SelectImageDialog_Load(object sender, EventArgs e) {
+            DrawList();
+        }
+
+        private void DrawList() {
+            int ItemIndex = 0;
+            foreach (Icon ico in IconList) {
+                Bitmap? IconBmp = null;
+                try {
+                    IconBmp = IconUtil.ToBitmap(ico);
+                } catch { continue; }
+                if (IconBmp == null) continue;
+
+                Panel IconItem = new Panel {
+                    Name = "IconItem" + ItemIndex,
+                    Size = new Size(128, 148),
+                    Tag = ItemIndex
+                };
+
+                PictureBox PB_Icon = new PictureBox {
+                    Name = "PbIcon" + ItemIndex,
+                    Tag = ItemIndex,
+                    Size = new Size(IconItem.Width, IconItem.Width),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Image = IconBmp,
+                    Location = new Point(0, 0)
+                };
+
+                Label LBL_Size = new Label {
+                    Name = "LblSize" + ItemIndex,
+                    Tag = ItemIndex,
+                    AutoSize = false,
+                    Size = new Size(IconItem.Width, 20),
+                    Location = new Point(0, PB_Icon.Size.Height),
+                    Text = $"{IconBmp.Size.Width}px x {IconBmp.Size.Height}px"
+                };
+
+                IconItem.Click += OnItemClick;
+                PB_Icon.Click += OnItemClick;
+                LBL_Size.Click += OnItemClick;
+
+                IconItem.Controls.Add(PB_Icon);
+                IconItem.Controls.Add(LBL_Size);
+
+                flowLayoutPanel1.Controls.Add(IconItem);
+
+                ItemIndex++;
+            }
+        }
+
+        private void OnItemClick(object? sender, EventArgs e) {
+            if (sender == null) return;
+            if (!(sender is Control)) return;
+            Control ctrl = (Control)sender;
+
+            int ItemIndex = Convert.ToInt32(ctrl.Tag);
+            this.SelectedIcon = IconList[ItemIndex];
+            SelectItemPanel(ItemIndex);
+        }
+
+        private void SelectItemPanel(int ItemIndex) {
+            UnselectAllItemsPanel();
+            foreach (Control ctrl in flowLayoutPanel1.Controls) {
+                if (!(ctrl is Panel)) continue;
+                Panel panel = (Panel)ctrl;
+
+                if (!(panel.Name.Contains("IconItem"))) { continue; }
+                string LoopItemIndexStr = panel.Name.Replace("IconItem", "");
+                int LoopItemIndex = -1;
+                try { LoopItemIndex = Convert.ToInt32(LoopItemIndexStr); } catch { continue; }
+                if (LoopItemIndex == ItemIndex) {
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+                    BTN_OK.Enabled = true;
+                    break;
+                }
+            }
+        }
+
+        private void UnselectAllItemsPanel() {
+            foreach (Control ctrl in flowLayoutPanel1.Controls) {
+                if (!(ctrl is Panel)) continue;
+                Panel panel = (Panel)ctrl;
+                if (!(panel.Name.Contains("IconItem"))) { continue; }
+                panel.BorderStyle = BorderStyle.None;
+            }
+        }
+
+        private void BTN_OK_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void BTN_Cancel_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
